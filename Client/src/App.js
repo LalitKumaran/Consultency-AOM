5e8f11c77847c0ab9735c25838d164dd169988d9
lalit <lalitkumaran1602@gmail.com>
2023-05-06 10:28:39 +0530

    0

--- Client/src/App.js
+++ Client/src/App.js
@@ -9,6 +9,7 @@ import {Shop} from './component/Shop/Shop'
 import {About} from './component/About/About'
 import {Contact} from './component/Contact/contact'
 import {AddProduct} from './component/Admin/new'
+import {UpdateProduct} from './component/Admin/UpdateProduct'
 import Payment from "./component/payment/Payment"
 function App() {
   return (
@@ -26,8 +27,8 @@ function App() {
       <Route path='/about' element={<About/>}></Route> 
       <Route path='/contact' element={<Contact/>}></Route> 
       <Route path="/admin/newproduct" element={<AddProduct/>}></Route>
+      <Route path="/admin/updateproduct/:pid" element={<UpdateProduct/>}></Route>
       <Route path="/payment" element={<Payment/>}></Route>
-
     </Routes>
     </main>
     <footer>
